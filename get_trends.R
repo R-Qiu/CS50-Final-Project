3b9c88815d9260cd961517be7c426aa9d6e9c616
Richard Qiu <rqiu@college.harvard.edu>
2018-12-01 20:20:28 -0500

    Generate list of date ranges

--- get_trends.R
+++ get_trends.R
@@ -5,12 +5,28 @@
 
 library(lubridate)
 library(gtrendsR)
+library(rlist)
 library(tidyverse)
 
 
 terms <- c("united states")
 locations <- c("US-NY", "US-RI")
-dates <- "2008-01-01 2017-12-31"
+date_range <- "2008-01-01 2017-12-31"
+
+dates <- str_split(date_range, " ")[[1]] %>% ymd()
+
+
+temp_date <- dates[1]
+dates_sep <- months(2)
+dates_list <- list()
+repeat{
+  dates_list <- list.append(dates_list, c(temp_date, temp_date %m+% dates_sep))
+  temp_date <- temp_date %m+% dates_sep
+  if (temp_date >= dates[2]) {
+    break
+  }
+}
+
 
 trends <- gtrends(keyword = terms, geo = locations, time = dates)[[1]]
 
