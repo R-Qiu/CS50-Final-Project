b568c2aca53fc0b1b82f9c30a0eec196aa543c37
Panuvat Chutichetpong <panuvat_chutichetpong@college.harvard.edu>
2018-12-01 21:15:26 -0500

    update main

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import pandas as pd
 from numpy import array
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+import re
 
 # load dataset // shape = (134740, 90)
-df = pd.read_csv('/Users/apple/PycharmProjects/CS50-Final-Project/NYC-CentralPark.csv')
+df = pd.read_csv('/Users/apple/PycharmProjects/CS50-Final-Project/weather_data/NYC-CentralPark.csv')
 
 # Filter out FM-16
 df = df.loc[df['REPORTTPYE'] != 'FM-16']
@@ -25,52 +26,151 @@ df.drop(columns = ['DATE'], inplace = True)
 df.set_index(df['date'], inplace = True)
 df.index = pd.to_datetime(df.index)
 
-# grouped = df.resample('D').sum()
-# print(grouped.head(10))
-
 
 # Extract valuable features
 
-# Find Temp range
-SOD['TempRange'] = SOD['DAILYMaximumDryBulbTemp'] - SOD['DAILYMinimumDryBulbTemp']
+# Temperature (range, average, standard deviation)
+tempRange = SOD['DAILYMaximumDryBulbTemp'] - SOD['DAILYMinimumDryBulbTemp']
 
-# Find Temp sd
-grouped = df.resample('D').std()
-print(grouped['HOURLYWETBULBTEMPF'])
-print(grouped['HOURLYDRYBULBTEMPF'])
+tempAve = df['DAILYAverageDryBulbTemp']
 
-# Find Sun time
-SOD['DAILYSunrise'] = pd.to_datetime(SOD['DAILYSunrise'], format='%H%M')
-SOD['DAILYSunset'] = pd.to_datetime(SOD['DAILYSunset'], format='%H%M')
-SOD['SunTime'] = SOD['DAILYSunset'] - SOD['DAILYSunrise']
+grouped_std = df.resample('D').std()
+print(grouped_std)
+tempSD = grouped_std['HOURLYWETBULBTEMPF']
+
+# Find Sun time in minutes
+DAILYSunrise = pd.to_datetime(SOD['DAILYSunrise'], format='%H%M')
+DAILYSunset = pd.to_datetime(SOD['DAILYSunset'], format='%H%M')
+SunTime = DAILYSunset - DAILYSunrise
+SunTime = SunTime.dt.total_seconds() / 60
+
+# Visibility (average and standard deviation)
+# grouped_ave =  df.groupby(df.index.day).mean()
+# visAve = grouped_ave['HOURLYVISIBILITY']
+# print(visAve)
+# visSD = grouped_std['HOURLYVISIBILITY']
+# print(visSD)
+# m = df.groupby(df.index.day, as_index=False)['HOURLYVISIBILITY'].mean()
+# print(m)
+
+# Wind Speed
+windSpeed = SOD['DAILYAverageWindSpeed']
+
+# Amount of Precipitation
+DAILYPrecip = SOD['DAILYPrecip']
 
 # Find Precipitation type using one hot encoding
-label = ['FG:01', 'FG+:02', 'TS:03', 'PL:04', 'GR:05', 'GL:06', 'DU:07', 'HZ:08', 'BLSN:09', 'FC:10',
-        'WIND:11', 'BLPY:12', 'BR:13', 'DZ:14', 'FZDZ:15', 'RA:16', 'FZRA:17', 'SN:18', 'UP:19', 'MIFG:21',
-        'FZFG:22']
-values = array(label)
-# integer encode
-label_encoder = LabelEncoder()
-integer_encoded = label_encoder.fit_transform(values)
-print(integer_encoded)
-# binary encode
-onehot_encoder = OneHotEncoder(sparse=False)
-integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-print(onehot_encoded)
+weatherTypes = {'FG:01':[], 'FG+:02':[], 'TS:03':[], 'PL:04':[], 'GR:05':[], 'GL:06':[],
+                'DU:07':[], 'HZ:08':[], 'BLSN:09':[], 'FC:10':[], 'WIND:11':[], 'BLPY:12':[], 'BR:13':[], 'DZ:14':[],
+                'FZDZ:15':[], 'RA:16':[], 'FZRA:17':[], 'SN:18':[], 'UP:19':[], 'MIFG:21':[], 'FZFG:22':[]}
 
-def foo(name):
-    for email in list_of_emails:
-        if name.lower() in email:
-            return email
+DAILYWeather = SOD['DAILYWeather'].tolist()
+DAILYWeather = [str(i) for i in DAILYWeather]
 
-df['Email'] = df['Name'].apply(foo)
+for word in DAILYWeather:
+    for key in weatherTypes:
+        result = word.find(key)
+        weatherTypes[key].append(1 if result != -1 else 0)
 
-# split = SOD['DAILYWeather'].str.split(" ", n = 1, expand = True)
-# SOD['DAILYWeather']
+encoded = pd.DataFrame.from_dict(weatherTypes)
 
-onehot = pd.get_dummies(SOD['DAILYWeather'], prefix=['type'])
-print(onehot)
+# Cloud Cover extraction
+# Convert Skycondition data from dataframe into a list
+cloudCondition = df['HOURLYSKYCONDITIONS'].tolist()
+
+# Parse all elements into type string
+cloudCondition = [str(i) for i in cloudCondition]
+
+# Create a dictionary to store the extracted result
+conditionExtracted = {'cloudCover':[]}
+
+for condition in cloudCondition:
+    # Extract the cloud cover number using regular expression on a string
+    matchObj = re.search( r'\d\d\s\d*$', condition, re.M|re.I)
+    # If data is not null, append the first 2 characters (i.e '02') into the dictionary
+    if matchObj:
+        foundString = matchObj.group()
+        conditionExtracted['cloudCover'].append(foundString[:2])
+    # If null, append '0'
+    else:
+        conditionExtracted['cloudCover'].append('0')
+
+# Convert dict into dataframe and turn values from string into float
+cloudCover = pd.DataFrame.from_dict(conditionExtracted)
+cloudCover['cloudCover'] = cloudCover['cloudCover'].astype(float)
+
+# Reset index into datetime to be able to group into daily data
+cloudCover.set_index(df.index, inplace=True, drop=True)
+
+# Find mean of daily data
+cloudCover_daily = cloudCover.resample('D').mean()
+
+
+# Split DailyWeather into 4 columns (max description)
+# split = SOD['DAILYWeather'].str.split(" ", n = 3, expand = True)
+# print(split)
+# print(type(split))
+
+# # Fill all None with Nan
+# split.fillna(value=pd.np.nan, inplace=True)
+#
+# category1 = pd.get_dummies(split[0])
+# category2 = pd.get_dummies(split[1])
+# category3 = pd.get_dummies(split[2])
+# category4 = pd.get_dummies(split[3])
+#
+# print(category1)
+# print(category2)
+# category1.join(category2, all = True)
+# print(category1)
+#
+# # first combine the splits into one long list
+# combine = pd.concat([split[0], split[1], split[2], split[3]], axis=0)
+# combineDF = pd.DataFrame({combine.index, combine})
+# # combine.reset_index(inplace=True, drop=True)
+#
+# print(type(combine))
+# category = pd.get_dummies()
+# print(category)
+
+# # Integer Encode the long list
+# label_encoder = LabelEncoder()
+# integer_encoded = label_encoder.fit(combine.astype(str))
+# print(label_encoder.classes_)
+#
+# # Binary encode the long list
+# onehot_encoder = OneHotEncoder(sparse=False)
+# integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+# onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+
+# # Split back into 5 different arrays and sum them to get one hot encoding
+# result = numpy.split(onehot_encoded, 5)
+# finalResult = result[0] + result[1] + result[2] + result[3] + result[4]
+#
+# # Delete Nan Column (last column)
+# finalResult = numpy.delete(finalResult, -1, axis=1)
+# print(finalResult)
+# print(finalResult.shape)
+
+
+# new_result = numpy.add(result[0], result[1], result[2], result[3], result[4])
+
+# combine = pd.concat([split[0], split[1], split[2], split[3], split[4]])
+# label_encoder = LabelEncoder()
+# SOD[] = label_encoder.fit_transform(combine)
+
+# label encode the combined series to ensure eveything is encoded
+# one hot transform each split individually
+# add the one hot encoded together
+# 0 1 0 1 0...etc. (sample)
+
+
+
+
+
+#
+# onehot = pd.get_dummies(SOD['DAILYWeather'], prefix=['type'])
+# print(onehot)
 
 # selected = grouped[['DAILYWeather','DAILYPrecip', 'DAILYAverageDryBulbTemp',
 #                     ]]
