a207c0919c1b77d1d251217e166f3bf5ec93a4dc
Panuvat Chutichetpong <panuvat_chutichetpong@college.harvard.edu>
2018-12-02 22:30:51 -0500

    update

--- main.py
+++ main.py
@@ -59,11 +59,13 @@ SunTime.reset_index(inplace = True, drop=True)
 
 # Wind Speed
 windSpeed = pd.Series(SOD['DAILYAverageWindSpeed'], name='windSpeed')
+windSpeed.fillna(0, inplace=True)
 windSpeed.reset_index(inplace = True, drop=True)
 
 # Amount of Precipitation
 DAILYPrecip = pd.Series(SOD['DAILYPrecip'], name='precipAmount')
 DAILYPrecip.replace('T', 0, inplace=True)
+DAILYPrecip.fillna(0, inplace=True)
 DAILYPrecip.reset_index(inplace = True, drop=True)
 
 # Find Precipitation type using one hot encoding
@@ -118,58 +120,103 @@ cloudCover_daily.drop(cloudCover_daily.index[-1], inplace = True)
 
 # Compile all features into a feature dataframe
 features = pd.concat([tempRange, tempAve, SunTime, windSpeed, DAILYPrecip, weatherEncoded, cloudCover_daily], axis=1)
+features.apply(pd.to_numeric)
 
-
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.pipeline import Pipeline
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+
+# Load Google trends data as labels
+gtrend = pd.read_csv('/Users/apple/PycharmProjects/CS50-Final-Project/trends_data/NYC_gtrends_2008-2017.csv')
+gtrend['date'] = pd.to_datetime(gtrend['date'])
+gtrend = gtrend.loc[gtrend['date'] <= '2017-12-31']
+gtrend = gtrend['hits']
+gtrend.apply(pd.to_numeric)
+
 
 # Split training (8 years) and testing data (last 2 years)
-X_train = features.iloc[:2922]
+X = features.iloc[:2922]
 X_test = features.tail(730)
 
+Y = gtrend.iloc[:2922]
+Y_test = gtrend.tail(730)
+
+# def baseline_model():
+# 	# create model
+# 	model = Sequential()
+# 	model.add(Dense(13, input_dim=27, kernel_initializer='normal', activation='relu'))
+# 	model.add(Dense(1, kernel_initializer='normal'))
+# 	# Compile model
+# 	model.compile(loss='mean_squared_error', optimizer='adam')
+# 	return model
+#
+# # fix random seed for reproducibility
+# seed = 7
+# numpy.random.seed(seed)
+# # evaluate model with standardized dataset
+# estimator = KerasRegressor(build_fn=baseline_model, epochs=10, batch_size=50, verbose=0)
+#
+# kfold = KFold(n_splits=2, random_state=seed)
+# results = cross_val_score(estimator, X, Y, cv=kfold)
+# print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
 # define 5-fold cross validation test
-kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=250)
+kfold = KFold(n_splits=5)
 
 i = 0
-for train, test in kfold.split(X, Y):
+for train, validate in kfold.split(X):
+    X_train = X.iloc[train]
+    X_validate = X.iloc[validate]
+
+    Y_train = Y.iloc[train]
+    Y_validate = Y.iloc[validate]
+
     # Creating the Neural Network:
     model = Sequential()
-    model.add(Dense(5, input_dim=27, kernel_initializer='uniform'))
-    model.add(BatchNormalization())
+    model.add(Dense(3, input_dim=27, kernel_initializer='uniform'))
     model.add(Activation('relu'))
 
-    model.add(Dense(5, kernel_initializer='uniform'))
-    model.add(BatchNormalization())
+    model.add(Dense(3, kernel_initializer='uniform'))
+    model.add(Activation('relu'))
+
+    model.add(Dense(1, kernel_initializer='uniform'))
     model.add(Activation('relu'))
 
     # Compiling the Model:
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='mean_squared_error', optimizer='adam')
 
     # Creating Callback function:
-    mcp = ModelCheckpoint(filepath="final_model_fold" + str(i) + "_weights.h5", monitor='val_loss', save_best_only=True, mode='min', verbose=1)
+    mcp = ModelCheckpoint(filepath="model_fold" + str(i) + "_weights.h5", monitor='val_loss', save_best_only=True, mode='min', verbose=1)
     # es = EarlyStopping(monitor='val_loss', min_delta=0, patience=30, verbose=0, mode='auto')
 
     # Fit the model
-    history = model.fit(X_train_final, Y[train], validation_data=(X_test_final, Y[test]), nb_epoch=200, batch_size=3,
-                        callbacks=[mcp], verbose=0)
+    history = model.fit(X_train, Y_train, validation_data=(X_validate, Y_validate), nb_epoch=10, batch_size=50,
+                        callbacks=[mcp], verbose=1)
 
     # load weights
-    model.load_weights("final_model_fold" + str(i) + "_weights.h5")
+    model.load_weights("model_fold" + str(i) + "_weights.h5")
     # Compiling the Model:
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    prediction = model.predict(X_test_final)
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    prediction = model.predict(X_validate)
 
-# fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
+    print('fold: %d' % i)
+    print(prediction)
+    print(Y_validate)
 
-# evaluate model with standardized dataset
-estimators = []
-estimators.append(( standardize , StandardScaler()))
-estimators.append(( mlp , KerasRegressor(build_fn=baseline_model, nb_epoch=50,
-batch_size=5, verbose=0)))
-pipeline = Pipeline(estimators)
+    i += 1
+
+    # # Compute ROC curve and area the curve
+    # fpr, tpr, thresholds = roc_curve(Y[test], prediction[:, 0])
+    # tprs.append(interp(mean_fpr, fpr, tpr))
+    # tprs[-1][0] = 0.0
+    # roc_auc = auc(fpr, tpr)
+    # aucs.append(roc_auc)
+    # plt.plot(fpr, tpr, lw=1, alpha=0.3,
+    #          label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
 
